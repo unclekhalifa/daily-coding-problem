e163bd67bca037ba25a5e7c9e22ecbe002e5f884
Ibrahim Idris <ibrahim.idris.developer@gmail.com>
2021-07-31 13:11:55 +0100

    Solution for challenge 1

---
+++ src/chapter1.js
@@ -0,0 +1,13 @@
+const chapter1 = {
+    challenge1: function (input) {
+        const output = [];
+        for (const [index] of input.entries()) {
+            const tmpArr = [...input];
+            tmpArr[index] = 1;
+            output.push(tmpArr.reduce((total, num) => total * num));
+        }
+        return output;
+    }
+};
+
+module.exports = chapter1;
