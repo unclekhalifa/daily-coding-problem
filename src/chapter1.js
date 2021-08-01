8fec69b06e808a6923b71ec347017bd5bcb68d4a
Khalifa S <ibrahim.idris.developer@gmail.com>
2021-08-01 10:03:29 +0100

    Implement challenge 2: locate smallest window to be sorted

--- src/chapter1.js
+++ src/chapter1.js
@@ -7,6 +7,19 @@ const chapter1 = {
             output.push(tmpArr.reduce((total, num) => total * num));
         }
         return output;
+    },
+
+    challenge2: (input) => {
+        const sortedInput = [...input].sort((a, b) => {
+            if (a < b) return -1;
+            if (a > b) return 1;
+            return 0;
+        });
+        const window = [];
+        for (const [index, value] of input.entries()) {
+            if (value !== sortedInput[index]) window.push(index);
+        }
+        return [window[0], window.slice(-1)[0]];
     }
 };
 
--- src/tests/chapter1.test.js
+++ src/tests/chapter1.test.js
@@ -1,10 +1,16 @@
 const {
-    challenge1
+    challenge1,
+    challenge2
 } = require("../chapter1");
 
 describe("Chapter 1: Arrays", () => {
+
     test("Should return product of all other elements in array save itself", () => {
         expect(challenge1([1, 2, 3, 4, 5])).toEqual([120, 60, 40, 30, 24]);
         expect(challenge1([3, 2, 1])).toEqual([2, 3, 6]);
+    });
+
+    test("Locate smallest window to be sorted", () => {
+        expect(challenge2([3, 7, 5, 6, 9])).toEqual([1, 3]);
     });
 });
