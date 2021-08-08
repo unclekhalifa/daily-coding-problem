189422290b408c726eed2db811b7f0795a077a2f
Khalifa S <ibrahim.idris.developer@gmail.com>
2021-08-08 20:22:20 +0100

    Start chapter 2: commit attempt

--- src/chapter2.js
+++ src/chapter2.js
@@ -1,19 +1,19 @@
 const chapter2 = {
     challenge1: (w, s) => {
-        
         const occurrences = [];
+        let wLength = w.length;
+        let currentStrOccurrencePosition = 0;
+
         let originalWord = s.slice();
         while (originalWord.indexOf(w) !== -1) {
             let index = originalWord.indexOf(w);
-            occurrences.push(originalWord.indexOf(w));
+            if (currentStrOccurrencePosition === 0) occurrences.push(index);
+            else occurrences.push(index + wLength);
+            currentStrOccurrencePosition = index + wLength;
             originalWord = originalWord.replace(w, "");
         }
+
         originalWord = s.split("").reverse().join("");
-        while (originalWord.indexOf(w) !== -1) {
-            let index = originalWord.indexOf(w);
-            occurrences.push(originalWord.indexOf(w));
-            originalWord = originalWord.replace(w, "");
-        }
 
         return occurrences;
     }
