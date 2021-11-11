e2d7c5fed6b733ce9bffdfc49ee2baf6ac0f11bd
Khalifa S <ibrahim.idris.developer@gmail.com>
2021-11-11 09:25:30 +0000

    Test push and pop functionality

--- src/tests/chapter4.test.js
+++ src/tests/chapter4.test.js
@@ -5,8 +5,14 @@ const {
 describe("Chapter 4: Stacks & Queues", () => {
 
     test("Implement a max stack", () => {
+        // Check push functionality
         challenge1.push(4);
         expect(challenge1.stack.length).toBe(1);
+
+        // Check pop functionality
+        expect(challenge1.pop()).toBe(4);
+        expect(challenge1.pop()).toBe(-1);
+
     });
 
 });
